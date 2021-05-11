23c01c70f971a5d5136bf4ebbe39b12b5b561384
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-11 09:43:59 +0530

    task cmpleted successfully :-)

--- form-app.js
+++ form-app.js
@@ -1,51 +1,46 @@
 // 'use strict'
 
-// let forms = getSavedForms()
+let forms = getSavedForms()
+
 let f1
-// let lastName
-// let email
-// let mobileNo
-// let address
+let f2
+let f3
+let f4
+let f5
 
 document.getElementById('firstname').addEventListener('change', (e) => {
     f1 = e.target.value
-    console.log(f1)
+})
+
+document.getElementById('lastname').addEventListener('change', (e) => {
+    f2 = e.target.value
+})
+
+document.getElementById('email').addEventListener('change', (e) => {
+    f3 = e.target.value
+})
+
+document.getElementById('mobileno').addEventListener('change', (e) => {
+    f4 = e.target.value
+})
+
+document.getElementById('address').addEventListener('change', (e) => {
+    f5 = e.target.value
 })
 
 
 
 document.getElementById('submit').addEventListener('click', () => {
-    
 
-    
+    forms.push({
+        id: uuidv4(),
+        firstName: f1,
+        lastName: f2,
+        email: f3,
+        mobileNo: f4,
+        address: f5
+    })
 
-    // document.getElementById('lastname').addEventListener('input', (e) => {
-    //     lastName = e.target.value
-    // })
-
-    // document.getElementById('email').addEventListener('input', (e) => {
-    //     email = e.target.value
-    //     e.target.value = ''
-    // })
-
-    // document.getElementById('mobileno').addEventListener('input', (e) => {
-    //     mobileNo = e.target.value
-    //     e.target.value = ''
-    // })
-
-    // document.getElementById('address').addEventListener('input', (e) => {
-    //     address = e.target.value
-    //     e.target.value = ''
-    // })
-
-    // forms.push({
-    //     id: uuidv4(),
-    //     firstName: firstName,
-    //     lastName: lastName,
-    //     email: email,
-    //     mobileNo: mobileNo,
-    //     address: address
-    // })
-
-    // saveForms(forms)
+    saveForms(forms)
+    alert('Form submitted successfully :-)')
 })
