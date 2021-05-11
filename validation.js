e744627b65899c4c8c041bb17ae8b24cd45ab8f1
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-11 16:09:10 +0530

    printing given input data on console

--- validation.js
+++ validation.js
@@ -90,3 +90,37 @@ submit.addEventListener('click', (e) => {
         alert("data submiteed");
     }
 })
+
+let f0
+let f1
+let f2
+let f3
+let f4
+
+firstName.addEventListener('change', (e) => {
+    f0 = e.target.value
+})
+
+lastName.addEventListener('change', (e) => {
+    f1 = e.target.value
+})
+
+email.addEventListener('change', (e) => {
+    f2 = e.target.value
+})
+
+mobileNo.addEventListener('change', (e) => {
+    f3 = e.target.value
+})
+
+address.addEventListener('change', (e) => {
+    f4 = e.target.value
+})
+
+submit.addEventListener('click', (e) => {
+    console.log(`first name: ${f0}`)
+    console.log(`last name: ${f1}`)
+    console.log(`email: ${f2}`)
+    console.log(`mobile number: ${f3}`)
+    console.log(`address: ${f4}`)
+})
